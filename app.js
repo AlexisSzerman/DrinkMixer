3b25c8b764aed4f8a934c9b541aa3479e353824a
Alexis Szerman <alexszer1986@gmail.com>
2022-12-08 20:35:15 -0300

    segunda preentrega agregando detalles

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+/*
 let bases = [];
 let diluyentes = [];
 
@@ -7,11 +9,13 @@ diluyentes.push (prompt ("Agregá un  diluyente (Coca, Sprite, Jugo de naranja o
 let coctelera = bases.concat(diluyentes);
 /* console.log(coctelera) */
 
-
+/*
 //Para saber que ingredientes agregué
 for (let i=0; i<=10; i++) {
   console.log (coctelera [i])
 }
+
+
 
 /*
 Sugerencia de Chatgpt
@@ -61,3 +65,57 @@ Sugerencia de Chatgpt
         }
         if (ingredientes.includes('limón') && ingredientes.includes('ron') && ingredientes.includes('hielo') && ingredientes.
         */
+
+
+
+
+/* Opcion chat gpt
+
+// Define an array of spirits
+let spirits = ['vodka', 'gin', 'rum', 'tequila', 'whiskey'];
+
+// Define an array of mixers
+let mixers = ['coca-cola', 'sprite', 'orange juice', 'tonic water'];
+
+// Use a loop to display a prompt for each spirit, allowing the user to select one
+let selectedSpirit = null;
+for (let i = 0; i < spirits.length; i++) {
+  let spirit = spirits[i];
+  let response = prompt(`Select a spirit: ${spirit}`);
+  if (response === 'yes') {
+    selectedSpirit = spirit;
+    break;
+  }
+}
+
+// Use a loop to display a prompt for each mixer, allowing the user to select one
+let selectedMixer = null;
+for (let i = 0; i < mixers.length; i++) {
+  let mixer = mixers[i];
+  let response = prompt(`Select a mixer: ${mixer}`);
+  if (response === 'yes') {
+    selectedMixer = mixer;
+    break;
+  }
+}
+
+// Use a switch statement to determine the recipe for the selected cocktail
+let recipe = '';
+switch (selectedSpirit) {
+  case 'vodka':
+    switch (selectedMixer) {
+      case 'coca-cola':
+        recipe = alert ("No Vodka and Coke Cocktails");
+        break;
+      case 'sprite':
+        recipe = alert ('Vodka and Lemonade: Pour 1.5 oz of vodka and 2 oz of lemonade into a glass with ice, stir, and enjoy!') ;
+        break;
+      case 'orange juice':
+        recipe = alert ('Screwdriver: Pour 1.5 oz of vodka and 2 oz of orange juice into a glass with ice, stir, and enjoy!');
+        break;
+      case 'tonic water':
+        recipe = alert ('Vodka Tonic: Pour 1.5 oz of vodka and 2 oz of tonic water into a glass with ice, stir, and enjoy!');
+        break
+    }
+  }
+  */
